a2611cbe3e587ed0661677b0b5fb751f29654d99
FirewoodCI <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-23 20:37:33 +0000

    firewood ci a123b66db9310ce733deb8dc9f3aac8032d072f8: attach firewood static libs

---
+++ ffi/src/handle.rs
@@ -0,0 +1,308 @@
+// Copyright (C) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE.md for licensing terms.
+
+use firewood::{
+    db::{Db, DbConfig},
+    manager::RevisionManagerConfig,
+    v2::api::{self, ArcDynDbView, Db as _, DbView, HashKey, HashKeyExt, IntoBatchIter, KeyType},
+};
+
+use crate::{BatchOp, BorrowedBytes, CView, CreateProposalResult, arc_cache::ArcCache};
+
+use crate::revision::{GetRevisionResult, RevisionHandle};
+use firewood_metrics::{MetricsContext, firewood_increment, firewood_record};
+
+/// The hashing mode to use for the database.
+///
+/// This determines the cryptographic hash function and trie structure used.
+#[repr(C)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum NodeHashAlgorithm {
+    /// MerkleDB Firewood hashing (SHA-256 based)
+    MerkleDB = 0,
+    /// Ethereum-compatible hashing (Keccak-256 based)
+    Ethereum = 1,
+}
+
+impl From<NodeHashAlgorithm> for firewood_storage::NodeHashAlgorithm {
+    fn from(alg: NodeHashAlgorithm) -> Self {
+        match alg {
+            NodeHashAlgorithm::MerkleDB => firewood_storage::NodeHashAlgorithm::MerkleDB,
+            NodeHashAlgorithm::Ethereum => firewood_storage::NodeHashAlgorithm::Ethereum,
+        }
+    }
+}
+
+/// Arguments for creating or opening a database. These are passed to [`fwd_open_db`]
+///
+/// [`fwd_open_db`]: crate::fwd_open_db
+#[repr(C)]
+#[derive(Debug)]
+pub struct DatabaseHandleArgs<'a> {
+    /// The path to the database directory.
+    ///
+    /// This must be a valid UTF-8 string.
+    ///
+    /// If this is empty, an error will be returned.
+    pub dir: BorrowedBytes<'a>,
+
+    /// Whether to enable `RootStore`.
+    ///
+    /// Note: Setting this feature will only track new revisions going forward
+    /// and will not contain revisions from a prior database instance that didn't
+    /// enable `root_store`.
+    pub root_store: bool,
+
+    /// The size of the node cache.
+    ///
+    /// Opening returns an error if this is zero.
+    pub cache_size: usize,
+
+    /// The size of the free list cache.
+    ///
+    /// Opening returns an error if this is zero.
+    pub free_list_cache_size: usize,
+
+    /// The maximum number of revisions to keep.
+    pub revisions: usize,
+
+    /// The cache read strategy to use.
+    ///
+    /// This must be one of the following:
+    ///
+    /// - `0`: No cache.
+    /// - `1`: Cache only branch reads.
+    /// - `2`: Cache all reads.
+    ///
+    /// Opening returns an error if this is not one of the above values.
+    pub strategy: u8,
+
+    /// Whether to truncate the database file if it exists.
+    pub truncate: bool,
+
+    /// Whether to enable expensive metrics recording for this database handle.
+    ///
+    /// Expensive metrics are disabled by default.
+    pub expensive_metrics: bool,
+
+    /// The hashing mode to use for the database.
+    ///
+    /// This must match the compile-time feature:
+    /// - [`NodeHashAlgorithm::Ethereum`] if the `ethhash` feature is enabled
+    /// - [`NodeHashAlgorithm::MerkleDB`] if the `ethhash` feature is disabled
+    ///
+    /// Opening returns an error if this does not match the compile-time feature.
+    pub node_hash_algorithm: NodeHashAlgorithm,
+}
+
+impl DatabaseHandleArgs<'_> {
+    fn as_rev_manager_config(&self) -> Result<RevisionManagerConfig, api::Error> {
+        let cache_read_strategy = match self.strategy {
+            0 => firewood::manager::CacheReadStrategy::WritesOnly,
+            1 => firewood::manager::CacheReadStrategy::BranchReads,
+            2 => firewood::manager::CacheReadStrategy::All,
+            _ => return Err(invalid_data("invalid cache strategy")),
+        };
+        let config = RevisionManagerConfig::builder()
+            .node_cache_size(
+                self.cache_size
+                    .try_into()
+                    .map_err(|_| invalid_data("cache size should be non-zero"))?,
+            )
+            .max_revisions(self.revisions)
+            .cache_read_strategy(cache_read_strategy)
+            .free_list_cache_size(
+                self.free_list_cache_size
+                    .try_into()
+                    .map_err(|_| invalid_data("free list cache size should be non-zero"))?,
+            )
+            .build();
+        Ok(config)
+    }
+}
+
+/// A handle to the database, returned by `fwd_open_db`.
+///
+/// These handles are passed to the other FFI functions.
+///
+#[derive(Debug)]
+#[repr(C)]
+pub struct DatabaseHandle {
+    /// A single cached view to improve performance of reads while committing
+    cached_view: ArcCache<HashKey, dyn api::DynDbView>,
+
+    /// The database
+    db: Db,
+
+    metrics_context: MetricsContext,
+}
+
+impl DatabaseHandle {
+    /// Creates a new database handle from the given arguments.
+    ///
+    /// # Errors
+    ///
+    /// If the path is empty, or if the configuration is invalid, this will return an error.
+    pub fn new(args: DatabaseHandleArgs<'_>) -> Result<Self, api::Error> {
+        let metrics_context = MetricsContext::new(args.expensive_metrics);
+
+        let cfg = DbConfig::builder()
+            .node_hash_algorithm(args.node_hash_algorithm.into())
+            .truncate(args.truncate)
+            .manager(args.as_rev_manager_config()?)
+            .root_store(args.root_store)
+            .build();
+
+        let path = args
+            .dir
+            .as_str()
+            .map_err(|err| invalid_data(format!("database path contains invalid utf-8: {err}")))?;
+
+        if path.is_empty() {
+            return Err(invalid_data("database path cannot be empty"));
+        }
+
+        let db = Db::new(path, cfg)?;
+        Ok(Self {
+            cached_view: ArcCache::new(),
+            db,
+            metrics_context,
+        })
+    }
+
+    /// Returns the current root hash of the database.
+    ///
+    /// # Errors
+    ///
+    /// An error is returned if there was an i/o error while reading the root hash.
+    pub fn current_root_hash(&self) -> Result<Option<HashKey>, api::Error> {
+        self.db.root_hash()
+    }
+
+    /// Returns a value from the database for the given key from the latest root hash.
+    ///
+    /// # Errors
+    ///
+    /// An error is returned if there was an i/o error while reading the value.
+    pub fn get_latest(&self, key: impl KeyType) -> Result<Option<Box<[u8]>>, api::Error> {
+        let Some(root) = self.current_root_hash()? else {
+            return Err(api::Error::RevisionNotFound {
+                provided: HashKey::default_root_hash(),
+            });
+        };
+
+        self.db.revision(root)?.val(key)
+    }
+
+    /// Creates a proposal with the given values and returns the proposal and the start time.
+    ///
+    /// # Errors
+    ///
+    /// An error is returned if the proposal could not be created.
+    pub fn create_batch<'a>(
+        &self,
+        values: impl AsRef<[BatchOp<'a>]> + 'a,
+    ) -> Result<Option<HashKey>, api::Error> {
+        let CreateProposalResult { handle, start_time } =
+            self.create_proposal_handle(values.as_ref())?;
+
+        let root_hash = handle.commit_proposal(|commit_time| {
+            firewood_increment!(crate::registry::COMMIT_MS, commit_time.as_millis());
+            firewood_record!(
+                crate::registry::COMMIT_MS_BUCKET,
+                commit_time.as_f64() * 1000.0,
+                expensive
+            );
+        })?;
+
+        let elapsed = start_time.elapsed();
+        firewood_increment!(crate::registry::BATCH_MS, elapsed.as_millis());
+        firewood_increment!(crate::registry::BATCH_COUNT, 1);
+        firewood_record!(
+            crate::registry::BATCH_MS_BUCKET,
+            elapsed.as_f64() * 1000.0,
+            expensive
+        );
+
+        Ok(root_hash)
+    }
+
+    /// Returns an owned handle to the revision corresponding to the provided root hash.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if could not get the view from underlying database for the specified
+    /// root hash, for example when the revision does not exist or an I/O error occurs while
+    /// accessing the database.
+    pub fn get_revision(&self, root: HashKey) -> Result<GetRevisionResult, api::Error> {
+        let view = self.db.view(root.clone())?;
+        Ok(GetRevisionResult {
+            handle: RevisionHandle::new(view, self.metrics_context),
+            root_hash: root,
+        })
+    }
+
+    pub(crate) fn get_root(&self, root: HashKey) -> Result<ArcDynDbView, api::Error> {
+        let mut cache_miss = false;
+        let view = self.cached_view.get_or_try_insert_with(root, |key| {
+            cache_miss = true;
+            self.db.view(HashKey::clone(key))
+        })?;
+
+        if cache_miss {
+            firewood_increment!(crate::registry::CACHED_VIEW_MISS, 1);
+        } else {
+            firewood_increment!(crate::registry::CACHED_VIEW_HIT, 1);
+        }
+
+        Ok(view)
+    }
+
+    pub(crate) fn clear_cached_view(&self) {
+        self.cached_view.clear();
+    }
+
+    pub(crate) fn merge_key_value_range(
+        &self,
+        first_key: Option<impl KeyType>,
+        last_key: Option<impl KeyType>,
+        key_values: impl IntoIterator<Item: api::KeyValuePair>,
+    ) -> Result<CreateProposalResult<'_>, api::Error> {
+        CreateProposalResult::new(self, || {
+            self.db
+                .merge_key_value_range(first_key, last_key, key_values)
+        })
+    }
+
+    /// Dumps the Trie structure of the latest revision to a DOT (Graphviz) format string.
+    ///
+    /// # Errors
+    ///
+    /// An error is returned if there was an i/o error while dumping the trie.
+    pub fn dump_to_string(&self) -> Result<String, api::Error> {
+        self.db.dump_to_string().map_err(api::Error::from)
+    }
+}
+
+impl<'db> CView<'db> for &'db crate::DatabaseHandle {
+    fn handle(&self) -> &'db crate::DatabaseHandle {
+        self
+    }
+
+    fn create_proposal(
+        self,
+        values: impl IntoBatchIter,
+    ) -> Result<firewood::db::Proposal<'db>, api::Error> {
+        self.db.propose(values)
+    }
+}
+
+impl crate::MetricsContextExt for DatabaseHandle {
+    fn metrics_context(&self) -> Option<MetricsContext> {
+        Some(self.metrics_context)
+    }
+}
+
+fn invalid_data(error: impl Into<Box<dyn std::error::Error + Send + Sync>>) -> api::Error {
+    api::Error::IO(std::io::Error::new(std::io::ErrorKind::InvalidData, error))
+}
---
+++ ffi/src/proofs/change.rs
@@ -0,0 +1,364 @@
+// Copyright (C) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE.md for licensing terms.
+
+#[cfg(feature = "ethhash")]
+use firewood::ProofError;
+#[cfg(feature = "ethhash")]
+use firewood_storage::TrieHash;
+#[cfg(feature = "ethhash")]
+use rlp::Rlp;
+
+use firewood::v2::api;
+
+use crate::{
+    BorrowedBytes, CResult, ChangeProofResult, DatabaseHandle, HashKey, HashResult, Maybe,
+    NextKeyRangeResult, OwnedBytes, ValueResult, VoidResult,
+};
+
+#[cfg(feature = "ethhash")]
+const EMPTY_CODE_HASH: [u8; 32] = [
+    // "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
+    0xc5, 0xd2, 0x46, 0x01, 0x86, 0xf7, 0x23, 0x3c, 0x92, 0x7e, 0x7d, 0xb2, 0xdc, 0xc7, 0x03, 0xc0,
+    0xe5, 0x00, 0xb6, 0x53, 0xca, 0x82, 0x27, 0x3b, 0x7b, 0xfa, 0xd8, 0x04, 0x5d, 0x85, 0xa4, 0x70,
+];
+
+/// Arguments for creating a change proof.
+#[derive(Debug)]
+#[repr(C)]
+pub struct CreateChangeProofArgs<'a> {
+    /// The root hash of the starting revision. This must be provided.
+    /// If the root is not found in the database, the function will return
+    /// [`ChangeProofResult::RevisionNotFound`].
+    pub start_root: HashKey,
+    /// The root hash of the ending revision. This must be provided.
+    /// If the root is not found in the database, the function will return
+    /// [`ChangeProofResult::RevisionNotFound`].
+    pub end_root: HashKey,
+    /// The start key of the range to create the proof for. If `None`, the range
+    /// starts from the beginning of the keyspace.
+    pub start_key: Maybe<BorrowedBytes<'a>>,
+    /// The end key of the range to create the proof for. If `None`, the range
+    /// ends at the end of the keyspace or until `max_length` items have been
+    /// included in the proof.
+    pub end_key: Maybe<BorrowedBytes<'a>>,
+    /// The maximum number of key/value pairs to include in the proof. If the
+    /// range contains more items than this, the proof will be truncated. If
+    /// `0`, there is no limit.
+    pub max_length: u32,
+}
+
+/// Arguments for verifying a change proof.
+#[derive(Debug)]
+#[repr(C)]
+pub struct VerifyChangeProofArgs<'a> {
+    /// The change proof to verify. If null, the function will return
+    /// [`VoidResult::NullHandlePointer`]. We need a mutable reference to
+    /// update the validation context.
+    pub proof: Option<&'a mut ChangeProofContext>,
+    /// The root hash of the starting revision. This must match the starting
+    /// root of the proof.
+    pub start_root: HashKey,
+    /// The root hash of the ending revision. This must match the ending root of
+    /// the proof.
+    pub end_root: HashKey,
+    /// The lower bound of the key range that the proof is expected to cover. If
+    /// `None`, the proof is expected to cover from the start of the keyspace.
+    pub start_key: Maybe<BorrowedBytes<'a>>,
+    /// The upper bound of the key range that the proof is expected to cover. If
+    /// `None`, the proof is expected to cover to the end of the keyspace.
+    pub end_key: Maybe<BorrowedBytes<'a>>,
+    /// The maximum number of key/value pairs that the proof is expected to cover.
+    /// If the proof contains more items than this, it is considered invalid. If
+    /// `0`, there is no limit.
+    pub max_length: u32,
+}
+
+/// FFI context for a parsed or generated change proof.
+#[derive(Debug)]
+pub struct ChangeProofContext {
+    _proof: (),              // currently not implemented
+    _validation_context: (), // placeholder for future use
+    _commit_context: (),     // placeholder for future use
+}
+
+/// A key range that should be fetched to continue iterating through a range
+/// or change proof that was truncated. Represents a half-open range
+/// `[start_key, end_key)`. If `end_key` is `None`, the range is unbounded
+/// and continues to the end of the keyspace.
+#[derive(Debug)]
+#[repr(C)]
+pub struct NextKeyRange {
+    /// The start key of the next range to fetch.
+    pub start_key: OwnedBytes,
+
+    /// If set, a non-inclusive upper bound for the next range to fetch. If not
+    /// set, the range is unbounded (this is the final range).
+    pub end_key: Maybe<OwnedBytes>,
+}
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub struct CodeIteratorHandle<'a> {
+    #[cfg(feature = "ethhash")]
+    inner: std::slice::Iter<'a, KeyValuePair>,
+    // uninhabitable fields make the struct impossible to construct when the feature is disabled
+    #[cfg(not(feature = "ethhash"))]
+    void: std::convert::Infallible,
+    #[cfg(not(feature = "ethhash"))]
+    marker: std::marker::PhantomData<&'a ()>,
+}
+
+type KeyValuePair = (Box<[u8]>, Box<[u8]>);
+
+impl Iterator for CodeIteratorHandle<'_> {
+    type Item = Result<HashKey, api::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        #[cfg(not(feature = "ethhash"))]
+        match self.void {}
+
+        #[cfg(feature = "ethhash")]
+        self.inner.find_map(|(key, value)| {
+            if key.len() != 32 {
+                return None;
+            }
+
+            let Ok(code_hash_slice) = Rlp::new(value).at(3).and_then(|r| r.data()) else {
+                return Some(Err(api::Error::ProofError(ProofError::InvalidValueFormat)));
+            };
+            let code_hash: HashKey = TrieHash::try_from(code_hash_slice).ok()?.into();
+            if code_hash == TrieHash::from(EMPTY_CODE_HASH).into() {
+                return None;
+            }
+
+            Some(Ok(code_hash))
+        })
+    }
+}
+
+impl<'a> CodeIteratorHandle<'a> {
+    /// Create a new code hash iterator from the given key/value pairs.
+    /// The key/value pairs should be the raw entries from the
+    /// underlying proof.
+    ///
+    /// The iterator must be freed after use.
+    ///
+    /// Arguments:
+    /// - `key_values` - The key/value pairs from the proof.
+    ///
+    /// Returns:
+    /// - `Ok(CodeIteratorHandle)` if the iterator was successfully created.
+    /// - `Err(api::Error)` if the iterator could not be created.
+    ///
+    /// # Errors
+    ///
+    /// - Returns `api::Error::FeatureNotSupported` if the `ethhash` feature
+    ///   is not enabled.
+    #[cfg_attr(feature = "ethhash", allow(clippy::missing_const_for_fn))]
+    #[cfg_attr(not(feature = "ethhash"), allow(unused_variables))]
+    pub fn new(key_values: &'a [KeyValuePair]) -> Result<Self, api::Error> {
+        #[cfg(not(feature = "ethhash"))]
+        {
+            Err(api::Error::FeatureNotSupported(
+                "ethhash code hash iterator".to_string(),
+            ))
+        }
+
+        #[cfg(feature = "ethhash")]
+        {
+            Ok(CodeIteratorHandle {
+                inner: key_values.iter(),
+            })
+        }
+    }
+}
+
+/// Create a change proof for the given range of keys between two roots.
+///
+/// # Arguments
+///
+/// - `db` - The database to create the proof from.
+/// - `args` - The arguments for creating the change proof.
+///
+/// # Returns
+///
+/// - [`ChangeProofResult::NullHandlePointer`] if the caller provided a null pointer.
+/// - [`ChangeProofResult::RevisionNotFound`] if the caller provided a start or end root
+///   that was not found in the database. The missing root hash is included in the result.
+///   The start root is checked first, and if both are missing, only the start root is
+///   reported.
+/// - [`ChangeProofResult::Ok`] containing a pointer to the `ChangeProofContext` if the proof
+///   was successfully created.
+/// - [`ChangeProofResult::Err`] containing an error message if the proof could not be created.
+#[unsafe(no_mangle)]
+pub extern "C" fn fwd_db_change_proof(
+    _db: Option<&DatabaseHandle>,
+    _args: CreateChangeProofArgs,
+) -> ChangeProofResult {
+    CResult::from_err("not yet implemented")
+}
+
+/// Verify a change proof and prepare a proposal to later commit or drop.
+///
+/// # Arguments
+///
+/// - `db` - The database to verify the proof against.
+/// - `args` - The arguments for verifying the change proof.
+///
+/// # Returns
+///
+/// - [`VoidResult::NullHandlePointer`] if the caller provided a null pointer to either
+///   the database or the proof.
+/// - [`VoidResult::Ok`] if the proof was successfully verified.
+/// - [`VoidResult::Err`] containing an error message if the proof could not be verified
+///
+/// # Thread Safety
+///
+/// It is not safe to call this function concurrently with the same proof context
+/// nor is it safe to call any other function that accesses the same proof context
+/// concurrently. The caller must ensure exclusive access to the proof context
+/// for the duration of the call.
+#[unsafe(no_mangle)]
+pub extern "C" fn fwd_db_verify_change_proof(
+    _db: Option<&DatabaseHandle>,
+    _args: VerifyChangeProofArgs,
+) -> VoidResult {
+    CResult::from_err("not yet implemented")
+}
+
+/// Verify and commit a change proof to the database.
+///
+/// If the proof has already been verified, the previously prepared proposal will be
+/// committed instead of re-verifying. If the proof has not been verified, it will be
+/// verified now. If the prepared proposal is no longer valid (e.g., the database has
+/// changed since it was prepared), a new proposal will be created and committed.
+///
+/// The proof context will be updated with additional information about the committed
+/// proof to allow for optimized introspection of the committed changes.
+///
+/// # Arguments
+///
+/// - `db` - The database to commit the changes to.
+/// - `args` - The arguments for verifying the change proof.
+///
+/// # Returns
+///
+/// - [`HashResult::NullHandlePointer`] if the caller provided a null pointer to either
+///   the database or the proof.
+/// - [`HashResult::None`] if the proof resulted in an empty database (i.e., all keys were deleted).
+/// - [`HashResult::Some`] containing the new root hash if the proof was successfully verified
+/// - [`HashResult::Err`] containing an error message if the proof could not be verified or committed.
+///
+/// # Thread Safety
+///
+/// It is not safe to call this function concurrently with the same proof context
+/// nor is it safe to call any other function that accesses the same proof context
+/// concurrently. The caller must ensure exclusive access to the proof context
+/// for the duration of the call.
+#[unsafe(no_mangle)]
+pub extern "C" fn fwd_db_verify_and_commit_change_proof(
+    _db: Option<&DatabaseHandle>,
+    _args: VerifyChangeProofArgs,
+) -> HashResult {
+    CResult::from_err("not yet implemented")
+}
+
+/// Returns the next key range that should be fetched after processing the
+/// current set of operations in a change proof that was truncated.
+///
+/// Can be called multiple times to get subsequent disjoint key ranges until
+/// it returns [`NextKeyRangeResult::None`], indicating there are no more keys to
+/// fetch and the proof is complete.
+///
+/// # Arguments
+///
+/// - `proof` - A [`ChangeProofContext`] previously returned from the create
+///   methods and has been prepared into a proposal or already committed.
+///
+/// # Returns
+///
+/// - [`NextKeyRangeResult::NullHandlePointer`] if the caller provided a null pointer.
+/// - [`NextKeyRangeResult::NotPrepared`] if the proof has not been prepared into
+///   a proposal nor committed to the database.
+/// - [`NextKeyRangeResult::None`] if there are no more keys to fetch.
+/// - [`NextKeyRangeResult::Some`] containing the next key range to fetch.
+/// - [`NextKeyRangeResult::Err`] containing an error message if the next key range
+///   could not be determined.
+///
+/// # Thread Safety
+///
+/// It is not safe to call this function concurrently with the same proof context
+/// nor is it safe to call any other function that accesses the same proof context
+/// concurrently. The caller must ensure exclusive access to the proof context
+/// for the duration of the call.
+#[unsafe(no_mangle)]
+pub extern "C" fn fwd_change_proof_find_next_key(
+    _proof: Option<&mut ChangeProofContext>,
+) -> NextKeyRangeResult {
+    CResult::from_err("not yet implemented")
+}
+
+/// Serialize a `ChangeProof` to bytes.
+///
+/// # Arguments
+///
+/// - `proof` - A [`ChangeProofContext`] previously returned from the create
+///   method. If from a parsed proof, the proof will not be verified before
+///   serialization.
+///
+/// # Returns
+///
+/// - [`ValueResult::NullHandlePointer`] if the caller provided a null pointer.
+/// - [`ValueResult::Some`] containing the serialized bytes if successful.
+/// - [`ValueResult::Err`] if the caller provided a null pointer.
+///
+/// The other [`ValueResult`] variants are not used.
+#[unsafe(no_mangle)]
+pub extern "C" fn fwd_change_proof_to_bytes(_proof: Option<&ChangeProofContext>) -> ValueResult {
+    CResult::from_err("not yet implemented")
+}
+
+/// Deserialize a `ChangeProof` from bytes.
+///
+/// # Arguments
+///
+/// * `bytes` - The bytes to deserialize the proof from.
+///
+/// # Returns
+///
+/// - [`ChangeProofResult::NullHandlePointer`] if the caller provided a null or zero-length slice.
+/// - [`ChangeProofResult::Ok`] containing a pointer to the `ChangeProofContext` if the proof
+///   was successfully parsed. This does not imply that the proof is valid, only that it is
+///   well-formed. The verify method must be called to ensure the proof is cryptographically valid.
+/// - [`ChangeProofResult::Err`] containing an error message if the proof could not be parsed.
+#[unsafe(no_mangle)]
+pub extern "C" fn fwd_change_proof_from_bytes(_bytes: BorrowedBytes) -> ChangeProofResult {
+    CResult::from_err("not yet implemented")
+}
+
+/// Frees the memory associated with a `ChangeProofContext`.
+///
+/// # Arguments
+///
+/// * `proof` - The `ChangeProofContext` to free, previously returned from any Rust function.
+///
+/// # Returns
+///
+/// - [`VoidResult::Ok`] if the memory was successfully freed.
+/// - [`VoidResult::Err`] if the process panics while freeing the memory.
+#[unsafe(no_mangle)]
+pub extern "C" fn fwd_free_change_proof(proof: Option<Box<ChangeProofContext>>) -> VoidResult {
+    crate::invoke_with_handle(proof, drop)
+}
+
+impl crate::MetricsContextExt for ChangeProofContext {
+    fn metrics_context(&self) -> Option<firewood_metrics::MetricsContext> {
+        None
+    }
+}
+
+impl crate::MetricsContextExt for CodeIteratorHandle<'_> {
+    fn metrics_context(&self) -> Option<firewood_metrics::MetricsContext> {
+        None
+    }
+}
